a2b577c892183b13d69673f9ad2b07924ff3bbb1
jun zhang <1282789783@qq.com>
2020-04-13 15:36:28 +0800

    添加绑定vbo步骤

---
+++ LearnOpenGL/LearnOpenGL.cpp
@@ -0,0 +1,41 @@
+﻿// LearnOpenGL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+void init() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+int main()
+{
+	init();
+
+	// 绑定顶点缓冲对象
+	unsigned int VBO;		// 创建定点缓冲对象变量
+	glGenBuffers(1, &VBO);	// 为变量绑定id号
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
